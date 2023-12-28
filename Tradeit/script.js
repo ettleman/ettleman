dae0dfb6a0f87257c3a2edf644d9ac4338f8f58d
ettleman <32594788+ettleman@users.noreply.github.com>
2023-12-28 21:34:43 +0000

    asd

--- Tradeit/script.js
+++ Tradeit/script.js
@@ -1,12 +1,9 @@
 function fetchStocks() {
-    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-    const targetUrl = 'https://tradestie.com/api/v1/apps/reddit';
-    fetch(proxyUrl + targetUrl)
+    fetch('https://tradestie.com/api/v1/apps/reddit')
         .then(response => response.json())
         .then(data => displayStocks(data))
         .catch(error => console.error('Error:', error));
 }
-
 
 function displayStocks(stocks) {
     const stocksDiv = document.getElementById('stocks');
